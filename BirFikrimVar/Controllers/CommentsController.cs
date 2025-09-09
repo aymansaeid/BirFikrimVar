fedb73c1e7461662c99e2834e02bca21e598958b
Ayman Saeid <aemon.saeid@gmail.com>
2025-09-10 02:18:08 +0300

    frontend mvc controller for comments

---
+++ BirFikrimVar/Controllers/CommentsController.cs
@@ -0,0 +1,100 @@
+﻿using BirFikrimVar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace BirFikrimVar.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly HttpClient _http;
+
+        public CommentsController(IHttpClientFactory httpClientFactory)
+        {
+            _http = httpClientFactory.CreateClient("ApiClient");
+        }
+
+        // GET: /Comments
+        public async Task<IActionResult> Index()
+        {
+            var comments = await _http.GetFromJsonAsync<List<CommentDto>>("api/CommentsApi");
+            return View(comments);
+        }
+
+        // GET: /Comments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Comments/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateCommentDto dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+
+            var response = await _http.PostAsJsonAsync("api/CommentsApi", dto);
+
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
+
+            ModelState.AddModelError("", "Failed to create comment.");
+            return View(dto);
+        }
+
+        // GET: /Comments/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var comment = await _http.GetFromJsonAsync<CommentDto>($"api/CommentsApi/{id}");
+            if (comment == null) return NotFound();
+
+            var dto = new UpdateCommentDto
+            {
+                Content = comment.Content
+            };
+
+            return View(dto);
+        }
+
+        // POST: /Comments/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, UpdateCommentDto dto)
+        {
+            if (!ModelState.IsValid)
+                return View(dto);
+
+            var response = await _http.PutAsJsonAsync($"api/CommentsApi/{id}", dto);
+
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
+
+            ModelState.AddModelError("", "Failed to update comment.");
+            return View(dto);
+        }
+
+        // GET: /Comments/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var comment = await _http.GetFromJsonAsync<CommentDto>($"api/CommentsApi/{id}");
+            if (comment == null) return NotFound();
+
+            return View(comment);
+        }
+
+        // POST: /Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var response = await _http.DeleteAsync($"api/CommentsApi/{id}");
+
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
+
+            return Problem("Failed to delete comment.");
+        }
+    }
+}
--- BirFikrimVar/Controllers/UsersController.cs
+++ BirFikrimVar/Controllers/UsersController.cs
@@ -1,156 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BirFikrimVar.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using BirFikrimVar.Models;
 
-namespace BirFikrimVar.Controllers
+public class UsersController : Controller
 {
-    public class UsersController : Controller
+    private readonly HttpClient _http;
+
+    public UsersController(IHttpClientFactory httpClientFactory)
     {
-        private readonly HttpClient _http;
+        _http = httpClientFactory.CreateClient("ApiClient");
+        // ApiClient must be configured in Program.cs with BaseAddress
+    }
 
-        public UsersController(IHttpClientFactory httpClientFactory)
+    // GET: /Users
+    public async Task<IActionResult> Index()
+    {
+        var users = await _http.GetFromJsonAsync<List<UserDto>>("api/UsersApi");
+        return View(users);
+    }
+
+    // GET: /Users/Register
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    // POST: /Users/Register
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterUserDto dto)
+    {
+        var response = await _http.PostAsJsonAsync("api/UsersApi/register", dto);
+        if (response.IsSuccessStatusCode)
         {
-            _http = httpClientFactory.CreateClient("ApiClient");
+            return RedirectToAction("Index");
         }
+        return View(dto);
+    }
 
-        // GET: Users
-        public async Task<IActionResult> Index()
+    // GET: /Users/Login
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    // POST: /Users/Login
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginUserDto dto)
+    {
+        var response = await _http.PostAsJsonAsync("api/UsersApi/login", dto);
+        if (response.IsSuccessStatusCode)
         {
-            return View(await _context.Users.ToListAsync());
+            var user = await response.Content.ReadFromJsonAsync<UserDto>();
+            // TODO: store user in session or cookie
+            TempData["Message"] = $"Welcome {user.FullName}";
+            return RedirectToAction("Index");
         }
+        ModelState.AddModelError("", "Invalid email or password.");
+        return View(dto);
+    }
 
-        // GET: Users/Details/5
-        public async Task<IActionResult> Details(int? id)
+    // GET: /Users/Edit/5
+    public async Task<IActionResult> Edit(int id)
+    {
+        var user = await _http.GetFromJsonAsync<UserDto>($"api/UsersApi/{id}");
+        if (user == null) return NotFound();
+        return View(user);
+    }
+
+    // POST: /Users/Edit/5
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, UpdateUserDto dto)
+    {
+        var response = await _http.PutAsJsonAsync($"api/UsersApi/{id}", dto);
+        if (response.IsSuccessStatusCode)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.UserId == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserId,FullName,Email,Password")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserId,FullName,Email,Password")] User user)
-        {
-            if (id != user.UserId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.UserId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.UserId == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user != null)
-            {
-                _context.Users.Remove(user);
-            }
-
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View(dto);
+    }
 
-        private bool UserExists(int id)
-        {
-            return _context.Users.Any(e => e.UserId == id);
-        }
+    // GET: /Users/Delete/5
+    public async Task<IActionResult> Delete(int id)
+    {
+        var user = await _http.GetFromJsonAsync<UserDto>($"api/UsersApi/{id}");
+        if (user == null) return NotFound();
+        return View(user);
+    }
+
+    // POST: /Users/Delete/5
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _http.DeleteAsync($"api/UsersApi/{id}");
+        return RedirectToAction(nameof(Index));
     }
 }
